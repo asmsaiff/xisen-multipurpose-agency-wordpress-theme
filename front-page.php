5af6948f1dd6d332af307a7bcabcb25d87aca537
saidmsaif <saidmsaiff@gmail.com>
2021-08-27 09:11:54 +0600

    add mail subscription form

--- front-page.php
+++ front-page.php
@@ -367,12 +367,20 @@
             </div>
             <div class="row">
                 <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                    <div class="newsletter">
-                        <form action="#">
-                            <input type="email" name="newsletter" id="newsletter" placeholder="Enter your email">
-                            <button type="submit">free trial</button>
+                    <!-- Begin Mailchimp Signup Form -->
+                    <div id="mc_embed_signup" class="newsletter">
+                        <form action="https://s-saif.us20.list-manage.com/subscribe/post?u=4eddfd595c7d4929892a3dbb7&amp;id=ad0b8645aa" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+                                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                    <input type="text" name="b_4eddfd595c7d4929892a3dbb7_ad0b8645aa" tabindex="-1" value="">
+                                </div>
+                                <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">Subscribe</button>
+                            </div>
                         </form>
                     </div>
+                    <!--End mc_embed_signup-->
                 </div>
             </div>
         </div>
