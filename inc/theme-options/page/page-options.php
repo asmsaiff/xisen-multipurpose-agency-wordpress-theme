f5b4659c973f48e30b50f72ddaefc334f7da1432
saidmsaif <saidmsaiff@gmail.com>
2021-08-27 15:53:40 +0600

    mailchimp form seperation and dynamic service page contents

--- inc/theme-options/page/page-options.php
+++ inc/theme-options/page/page-options.php
@@ -38,3 +38,32 @@
             ),
         )
     ) );
+
+    // Service Page
+    Redux::setSection( $opt_name, array(
+        'title'  => esc_html__( 'Service Page Options', 'xisen' ),
+        'id'     => 'service_page',
+        // 'icon'   => 'el el-th-large',
+        'subsection'    =>  true,
+        'fields' => array(
+            array(
+                'id'        =>  'service_page_features_section_title',
+                'type'      =>  'text',
+                'title'     =>  __('Heading', $opt_name), 
+                'subtitle'  =>  __('Service Page Features Section Heading Text', $xisen_textdomain),
+            ),
+            array(
+                'id'        =>  'service_page_features_section_subtitle',
+                'type'      =>  'text',
+                'title'     =>  __('Sub Title', $opt_name), 
+                'subtitle'  =>  __('Service Page Features Section Subtitle', $xisen_textdomain),
+            ),
+            array(
+                'id'        =>  'service_page_features_section_under_header_divider',
+                'type'      =>  'media',
+                'url'       =>  true,
+                'title'     =>  __('Divider Image', $opt_name), 
+                'subtitle'  =>  __('Upload a transparent divider line image', $xisen_textdomain),
+            ),
+        )
+    ) );
