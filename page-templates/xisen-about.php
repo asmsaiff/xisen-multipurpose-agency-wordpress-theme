e28f011cfa4a1f9a05be7e17e952dad5cf74b14f
saidmsaif <saidmsaiff@gmail.com>
2021-09-02 20:59:06 +0600

    about page brands area dynamic

--- page-templates/xisen-about.php
+++ page-templates/xisen-about.php
@@ -151,57 +151,27 @@
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-title-2 mb-70 text-center">
-                        <h2 class="mb-20">Trusted over 100+ companies</h2>
-                        <p>Him have deep brought life darkness firmament unto move</p>
+                        <h2 class="mb-20"><?php echo get_xisen_redux_data('about_page_brand_area_title'); ?></h2>
+                        <p><?php echo get_xisen_redux_data('about_page_brand_area_subtitle'); ?></p>
                     </div>
                 </div>
             </div>
             <div class="row justify-content-center">
+                <?php
+                    global $xisen_options;
+
+                    $logo = $xisen_options['about_page_brand_area_logos'];
+                    $get_post_thumb         =   explode(",", $logo);
+
+                    foreach($get_post_thumb as $image) :
+                            $thumb_url = wp_get_attachment_image_src($image);
+                ?>
                 <div class="logo-brand-item">
                     <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/01.png" alt=""></a>
+                        <img src="<?php echo $thumb_url[0]; ?>" alt="">
                     </div>
                 </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/02.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/03.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/03 (1).png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/04.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/05.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/03.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/03 (1).png" alt=""></a>
-                    </div>
-                </div>
-                <div class="logo-brand-item">
-                    <div class="brand-img">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand/02.png" alt=""></a>
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
